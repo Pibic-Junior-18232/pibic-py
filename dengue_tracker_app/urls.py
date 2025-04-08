92d1088e68b29a5066a0df74ff3c81ac14e389ac
Marcelo Gonçalves <joaogedavim787@gmail.comgit config --global user.name Marcelo>
2025-04-08 20:58:47 -0300

    feat: add page to list cases registered

--- dengue_tracker_app/urls.py
+++ dengue_tracker_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from dengue_tracker_app import views
 
 urlpatterns = [
-    path("", views.home_page, name="main_page")
+    path("", views.home_page, name="home"),
+    path("registros/", views.list_cases, name="registros")
 ]
--- dengue_tracker_app/views.py
+++ dengue_tracker_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+def list_cases(request):
+    return render (request, 'cases_registered.html')
