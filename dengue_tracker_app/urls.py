b407e2548aa5f4f4d91e032b2c41f7cf3ebe29e1
Marcelo Gonçalves <joaogedavim787@gmail.comgit config --global user.name Marcelo>
2025-04-14 06:19:11 -0300

    feat: add methods to edit and delete cases

--- dengue_tracker_app/urls.py
+++ dengue_tracker_app/urls.py
@@ -4,5 +4,7 @@ from dengue_tracker_app import views
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("registros/", views.list_cases, name="registros"),
-    path("novo_caso/", views.register_case, name="novo_caso"),
+    path("form_case/", views.case_register_or_edit, name="new_case"),
+    path("form_case/<int:case_id>/", views.case_register_or_edit, name="edit_case"),
+    path("delete/<int:case_id>/", views.case_delete, name="delete_case"),
 ]
--- dengue_tracker_app/views.py
+++ dengue_tracker_app/views.py
@@ -1,46 +1,78 @@
-from django.shortcuts import render, redirect
-from .models import Case
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Case, Patient, Address
 from .forms import PatientForm, AddressForm, CaseForm
 from django.db import transaction
+from django.utils import timezone
 
 def home_page(request):
     return render(request, 'home_page.html')
 
 def list_cases(request):
-    cases = Case.objects.all()
+    cases = Case.objects.filter(deleted=False).order_by('-created_at')
     return render (request, 'cases_registered.html', {"cases": cases})
 
 
-def register_case(request):
-    
+def case_register_or_edit(request, case_id=None): # Funcao responsavel pela criacao e a edicao de casos.
+
+    if case_id:
+        case = get_object_or_404(Case, id=case_id, deleted=False) # Procura um objeto caso nao encontre retorna 404
+        patient = case.patient
+        address = case.address
+
+    else: 
+        case = None
+        patient = None
+        address = None
+
     if request.method == 'POST':
-        patient_form = PatientForm(request.POST)
-        address_form = AddressForm(request.POST)
-        case_form = CaseForm(request.POST)
-        
+        case_form = CaseForm(request.POST, instance=case)
+        patient_form = PatientForm(request.POST, instance=patient)
+        address_form = AddressForm(request.POST, instance=address)
+
         if all([patient_form.is_valid(), address_form.is_valid(), case_form.is_valid()]):
-            patient = patient_form.save()
-            address = address_form.save(commit=False)
-            address.patient = patient
-            address.save()
-            
-            case = case_form.save(commit=False)
-            case.patient = patient
-            case.address = address
-            case.save()
-            case_form.save_m2m()  # para salvar os sintomas
 
-            return redirect('registros')
+            try: # Tente fazer tudo que esta ai dentro se der erro aborta tudo
+                with transaction.atomic():
+
+                    patient = patient_form.save()
+                    address = address_form.save()
+
+                    case = case_form.save(commit=False)
+
+                    case.address = address
+                    case.patient = patient
+
+                    case.save()
+                    case_form.save_m2m()
+        
+                return redirect('registros')
+
+            except Exception as ex:
+                print("Erro: ", e)    
     else:
-        patient_form = PatientForm()
-        address_form = AddressForm()
-        case_form = CaseForm()
+        case_form = CaseForm(instance=case)
+        patient_form = PatientForm(instance=patient)
+        address_form = AddressForm(instance=address)
 
-    
-    context = {
+    return render(request, 'form_case.html', {
+        'case_form': case_form,
         'patient_form': patient_form,
         'address_form': address_form,
-        'case_form': case_form,
-    }
+        'mode_edit': case_id is not None, 
+    })
 
-    return render(request, 'register_case.html', context)
+
+def case_delete(request, case_id):
+    
+    case = get_object_or_404(Case, id=case_id, deleted=False)
+    address = get_object_or_404(Address, id=case.address.id, deleted=False)
+
+    address.deleted = True
+    address.deleted_at = timezone.now()
+    address.save()
+
+    case.deleted = True
+    case.deleted_at = timezone.now()
+    case.save()
+
+    return redirect('registros')    
